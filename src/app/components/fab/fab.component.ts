abd6055b698c61c024b1d033718961ff8a955516
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-09-28 22:06:19 +0500

    add fab animation

--- src/app/components/fab/fab.component.ts
+++ src/app/components/fab/fab.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInRight } from 'src/app/animations/fab.animation';
 
 @Component({
   selector: 'app-fab',
   templateUrl: './fab.component.html',
-  styleUrls: ['./fab.component.scss']
+  styleUrls: ['./fab.component.scss'],
+  animations: [
+    slideInRight
+  ]
 })
 export class FabComponent implements OnInit {
 

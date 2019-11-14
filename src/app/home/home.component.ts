1cf393acae0bef25c07066bc10f3f0dcebe1cd98
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-11-14 10:33:45 +0500

    add Twitter/FB meta tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AboutComponent } from '../about/about.component';
+import { Meta, Title } from '@angular/platform-browser';
+import { TAB_TITLE } from '../global';
 
 @Component({
   selector: 'app-home',
@@ -9,54 +9,14 @@ import { AboutComponent } from '../about/about.component';
 })
 export class HomeComponent implements OnInit {
 
-  subscription: Subscription;
-
-  typeComplete = false;
-
-  projects: any = [
-    {
-      name: 'historia',
-      type: 'Open Source',
-      description: `A python program that uses python's built-in sqlite3
-      module to implement support for temporal tables in SQLite`,
-      link: 'https://www.github.com/ShehriyarQureshi/historia',
-      image: 'assets/images/temporal-table.png'
-    },
-    {
-      name: 'Tuition Manager',
-      type: 'Open Source',
-      image: 'assets/images/tuition-manager.png',
-      description: `This was my first ever big programming project. I started
-      learning to code in Python and I developed this project to get an
-      understanding of programming basics and how I/O and stuff works`,
-      link: 'https://www.github.com/ShehriyarQureshi/tuition-manager'
-    },
-    {
-      name: 'employeeManager',
-      type: 'Open Source',
-      image: 'assets/images/employeeManager.png',
-      description: `My first Java program that uses JSF, Hibernate & Spring
-      Security. Not proud of it except those Pokemon`,
-      link: 'https://www.github.com/ShehriyarQureshi/employeeManager'
-
-    },
-    {
-      name: 'Mobile Connect Profile',
-      type: 'Open Source',
-      description: `Designed & Developed the base for GSMA Mobile Connect Profile prototype`,
-      link: 'https://www.github.com/ShehriyarQureshi/mobile-connect-profile',
-      image: 'assets/images/gsma-mobile-connect.jpg'
-    }
-  ];
-
-  constructor(public about: AboutComponent) {
+  constructor(
+    private meta: Meta,
+    private title: Title
+  ) {
   }
 
   ngOnInit() {
-  }
-
-  goToLink(link: string): void {
-    window.open(link);
+    this.title.setTitle(TAB_TITLE.HOME);
   }
 
 }

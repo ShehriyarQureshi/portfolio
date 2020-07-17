5d508fa04cd033ab8a700899a045a4737ec45535
Shehriyar Qureshi <SShehriyar266@gmail.com>
2020-07-18 00:16:05 +0500

    noice words about technologies I'm comfortable with

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -8,19 +8,27 @@ export class ProjectService {
   technologies: Array<any> = [
     {
       title: 'Rust',
-      description: 'building networking tools'
+      description: `
+      My first systems programming experience related to
+      networking tools dealing with OSI Layer 3.
+      `
     },
     {
       title: 'Angular',
-      description: 'dashboards & this website'
+      description: `
+      Built SaaS dashboards and consumed GIS APIs written in TypeScript.
+      `
     },
     {
       title: 'Java',
-      description: 'backend APIs'
+      description: 'Built backend APIs to be consumed by other developers.'
     },
     {
       title: 'Python',
-      description: 'general purpose programming'
+      description: `
+      Used pure Python for learning programming and building hobby projects
+      such as adding support for Temporal Tables in sqlite.
+      `
     },
   ];
 
